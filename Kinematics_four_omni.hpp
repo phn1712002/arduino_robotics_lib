c58d5191565d3702ac58a970829ae032450dc8ab
Hoanggg Nam <phn1712002@gmail.com>
2024-11-08 11:04:30 +0700

    feat: :white_check_mark: New code

--- Kinematics_four_omni.hpp
+++ Kinematics_four_omni.hpp
@@ -1,11 +1,14 @@
+/*
+    Read: https://github.com/MasdikaAliman/Kinematic-Omniwheels-in-Webots/blob/master/controllers/4omni_wheels/Kinematik.cpp
+*/
 #include "Arduino.h"
 
 class Kinematics
 {
 public:
-    Kinematics(float bodyWheelOffsetMeters, float wheelRadiusMeters) : 
-        _bodyWheelOffsetMeters(bodyWheelOffsetMeters),
-        _wheelRadiusMeters(wheelRadiusMeters)
+    Kinematics(float platform_wheel_offset_meters, float wheel_radius_meters) : 
+        _platform_wheel_offset_meters(platform_wheel_offset_meters),
+        _wheel_radius_meters(wheel_radius_meters)
     {
     }
 
@@ -23,8 +26,8 @@ public:
 
     rpm get_rpm(float linear_x, float linear_y, float angular_z);
 
-    float _bodyWheelOffsetMeters;
-    float _wheelRadiusMeters;
+    float _platform_wheel_offset_meters;
+    float _wheel_radius_meters;
 };
 
 // Receive desired platform velocity and compute the required RPM for each motor to achieve this velocity
@@ -37,10 +40,10 @@ Kinematics::rpm Kinematics::get_rpm(float linearX, float linearY, float angularZ
 {
     const auto angularZRadians = float(M_PI) * angularZ / 180;
 
-    auto u1 = (-cos(_bodyWheelOffsetMeters * M_PI / 180) * linearX + sin(_bodyWheelOffsetMeters * M_PI / 180) * linearY + L * angularZRadians) / _wheelRadiusMeters;
-    auto u2 = (-cos(_bodyWheelOffsetMeters * M_PI / 180) * linearX - sin(_bodyWheelOffsetMeters * M_PI / 180) * linearY + L * angularZRadians) / _wheelRadiusMeters;
-    auto u3 = (cos(_bodyWheelOffsetMeters * M_PI / 180) * linearX - sin(_bodyWheelOffsetMeters * M_PI / 180) * linearY + L * angularZRadians) / _wheelRadiusMeters;
-    auto u4 = (cos(_bodyWheelOffsetMeters * M_PI / 180) * linearX + sin(_bodyWheelOffsetMeters * M_PI / 180) * linearY + L * angularZRadians) / _wheelRadiusMeters;
+    auto u1 = (-cos(_platform_wheel_offset_meters * M_PI / 180) * linearX + sin(_platform_wheel_offset_meters * M_PI / 180) * linearY + L * angularZRadians) / _wheel_radius_meters;
+    auto u2 = (-cos(_platform_wheel_offset_meters * M_PI / 180) * linearX - sin(_platform_wheel_offset_meters * M_PI / 180) * linearY + L * angularZRadians) / _wheel_radius_meters;
+    auto u3 = (cos(_platform_wheel_offset_meters * M_PI / 180) * linearX - sin(_platform_wheel_offset_meters * M_PI / 180) * linearY + L * angularZRadians) / _wheel_radius_meters;
+    auto u4 = (cos(_platform_wheel_offset_meters * M_PI / 180) * linearX + sin(_platform_wheel_offset_meters * M_PI / 180) * linearY + L * angularZRadians) / _wheel_radius_meters;
 
     return {{u1, u2, u3, u4}};
 };
--- Kinematics_three_omni.hpp
+++ Kinematics_three_omni.hpp
@@ -13,33 +13,31 @@
 //         See the License for the specific language governing permissions and
 //         limitations under the License.
 
+// Read : https://github.com/avivhu/botabotabot/blob/main/src/kinematics.hpp
+
 #include "Arduino.h"
 
 class Kinematics
 {
 public:
-    Kinematics(float bodyWheelOffsetMeters, float wheelRadiusMeters) : 
-        _bodyWheelOffsetMeters(bodyWheelOffsetMeters),
-        _wheelRadiusMeters(wheelRadiusMeters)
-    {
-    }
-
+    Kinematics(float platform_wheel_offset_meters, float wheel_radius_meters, float max_motor_rpm);
+    
     struct rpm
     {
         float motorRpm[3];
     };
 
-    struct velocities
-    {
-        float linear_x;
-        float linear_y;
-        float angular_z;
-    };
-	
     rpm get_rpm(float linear_x, float linear_y, float angular_z);
+    rpm get_rpm_use_joystick(float joystick_linearX, float joystick_linearY, float joystick_angularZ);
 
-    float _bodyWheelOffsetMeters;
-    float _wheelRadiusMeters;
+private:
+    float _max_motor_rpm;
+    float _platform_wheel_offset_meters;
+    float _wheel_radius_meters;
+    float _max_wheel_rotation_velocity_radiansPerSec;
+    float _max_translation_velocity_metersPerSec;
+    float _max_platform_rotation_velocity_radiansPerSec;
+    float _max_platform_rotation_velocity_degreesPerSec;
 };
 
 // Receive desired platform velocity and compute the required RPM for each motor to achieve this velocity
@@ -48,16 +46,27 @@ public:
 // => The platform's positive rotation is Counter Clock-Wise as seen from the top looking down.
 // => The wheels' positive rotation is Clock-Wise as seen from inside the platform looking out.
 
+Kinematics::Kinematics(float platform_wheel_offset_meters, float wheel_radius_meters, float max_motor_rpm)
+{
+    this->_wheel_radius_meters = wheel_radius_meters;
+    this->_platform_wheel_offset_meters = platform_wheel_offset_meters;
+    this->_max_motor_rpm = max_motor_rpm;
+
+    this->_max_wheel_rotation_velocity_radiansPerSec = this->_max_motor_rpm * (2 * M_PI) / 60;
+    this->_max_translation_velocity_metersPerSec = this->_wheel_radius_meters * this->_max_wheel_rotation_velocity_radiansPerSec;
+    this->_max_platform_rotation_velocity_radiansPerSec = this->_max_translation_velocity_metersPerSec / this->_platform_wheel_offset_meters;
+    this->_max_platform_rotation_velocity_degreesPerSec = this->_max_platform_rotation_velocity_radiansPerSec * 180 / M_PI;
+}
+
 Kinematics::rpm Kinematics::get_rpm(float linearX, float linearY, float angularZ)
 {
     const auto sin_60 = 0.8660254037844386f;
-
     const auto angularZRadians = float(M_PI) * angularZ / 180;
 
     // Compute angular speed of each wheel in radians / sec
-    auto u1 = (-_bodyWheelOffsetMeters * angularZRadians + 1.0f * linearX) / _wheelRadiusMeters;
-    auto u2 = (-_bodyWheelOffsetMeters * angularZRadians - 0.5f * linearX + -sin_60 * linearY) / _wheelRadiusMeters;
-    auto u3 = (-_bodyWheelOffsetMeters * angularZRadians - 0.5f * linearX + +sin_60 * linearY) / _wheelRadiusMeters;
+    auto u1 = (-this->_platform_wheel_offset_meters * angularZRadians + 1.0f * linearX) / this->_wheel_radius_meters;
+    auto u2 = (-this->_platform_wheel_offset_meters * angularZRadians - 0.5f * linearX + -sin_60 * linearY) / this->_wheel_radius_meters;
+    auto u3 = (-this->_platform_wheel_offset_meters * angularZRadians - 0.5f * linearX + +sin_60 * linearY) / this->_wheel_radius_meters;
 
     // Convert to rad/sec to RPM (revs per minute)
     u1 *= 60.0 / (2 * M_PI);
@@ -66,3 +75,16 @@ Kinematics::rpm Kinematics::get_rpm(float linearX, float linearY, float angularZ
 
     return {{u1, u2, u3}};
 };
+
+Kinematics::rpm Kinematics::get_rpm_use_joystick(float joystick_linearX, float joystick_linearY, float joystick_angularZ)
+{
+    /*
+    joystick_linearX, joystick_linearX, joystick_linearX = [-1, 1] is value of joystick
+    */
+    auto dx = joystick_linearX * this->_max_translation_velocity_metersPerSec;
+    auto dy = joystick_linearY * this->_max_translation_velocity_metersPerSec;
+    auto dRot = joystick_angularZ * this->_max_platform_rotation_velocity_degreesPerSec;
+    Kinematics::rpm all_rpm = this->get_rpm(dx, dy, dRot);
+
+    return all_rpm;
+}
